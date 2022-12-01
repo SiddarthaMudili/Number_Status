9aef6bab501cf8c5a5fcf7a18139ef5c3c761ced
SiddarthaMudili <babusravansiddartha@lpu.in>
2022-12-01 22:06:39 +0530

    Update Number_Status.py

--- Number_Status.py
+++ Number_Status.py
@@ -3,20 +3,20 @@ A=int(input("Enter lowest number of the Range: "))
 B=int(input("Enter Highest Number of the Range: "))
 print("In the Range of Numbers","["+str(A)+","+str(B)+"]")
 for num in range(A,B+1):
-    flag = False
+    flag = False #num is a Prime number
     if num>1:
         for i in range(2,num):
             if (num%i)==0:
-                flag=True
+                flag=True #num is a Composite number
                 break
     if flag:
         print(num,"is Composite Number")
     else:
-        if num>0:
-            if num==1:
-                print(num,"Neither Prime Nor Composite")
+        if num>=0:
+            if num==1 or num==0:
+                print(num,"Neither Prime Nor Composite") #Since 1 and 0 are neither prime nor composite
             else:
                 print(num,"is Prime Number")
         else:
-            print("Invalid Input")
+            print("Invalid Input") #Values below 0 are not considered
             break
